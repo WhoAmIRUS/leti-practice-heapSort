41766d3eac2fae94565ba59eae65ae74de5117fa
Dima Glazkov <glazkov.forscience@gmail.com>
2018-07-09 11:08:46 +0300

    final version

--- heapsort/view/Graphics.java
+++
@@ -1,7 +0,0 @@
-package heapsort.view;
-
-public interface Graphics {
-    void drawOval(int x, int y, int width, int height, int rgb);
-    void drawLine(int x1, int y1, int x2, int y2, int rgb);
-    void drawText(int x, int y, String text, int rgb);
-}
